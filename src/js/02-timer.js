a4a4b7c6830aa3ac51315534a4f20e4c1b15ad55
Sebastian Hanusiak <drais234@gmail.com>
2021-09-17 10:07:07 +0200

    js1, js2

---
+++ src/js/02-timer.js
@@ -0,0 +1,100 @@
+import flatpickr from 'flatpickr';
+import { Notify } from 'notiflix';
+
+import 'flatpickr/dist/flatpickr.min.css';
+import 'notiflix/dist/notiflix-3.0.2.min.css';
+
+const timerOn = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  daysLeft: document.querySelector('[data-days]'),
+  hoursLeft: document.querySelector('[data-hours]'),
+  minutesLeft: document.querySelector('[data-minutes]'),
+  secondsLeft: document.querySelector('[data-seconds]'),
+};
+timerOn.startBtn.disabled = true;
+let timeSelected;
+let intervalId;
+let isActive = false;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    timeSelected = Date.parse(selectedDates);
+    if (timeSelected - Date.now() <= 0) {
+      Notify.failure('Please choose a date in the future', { clickToClose: true });
+      return;
+    } else {
+      timerOn.startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr('#date-selector', options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function start() {
+  if (isActive) {
+    return;
+  }
+
+  intervalId = setIntervalImmediately(timeUpdate, 1000);
+  isActive = true;
+}
+
+function timeUpdate() {
+  const timeLeft = timeSelected - Date.now();
+  if (timeLeft < 0) {
+    clearInterval(intervalId);
+    // isActive = false;
+    return;
+  }
+  const time = convertMs(timeLeft);
+  updateInterface(time);
+}
+
+function stop() {
+  // isActive = false;
+  clearInterval(intervalId);
+}
+
+function updateInterface({ days, hours, minutes, seconds }) {
+  timerOn.daysLeft.textContent = changes(days);
+  timerOn.hoursLeft.textContent = changes(hours);
+  timerOn.minutesLeft.textContent = changes(minutes);
+  timerOn.secondsLeft.textContent = changes(seconds);
+}
+
+function changes(value) {
+  return String(value).padStart(2, '0');
+}
+
+function setIntervalImmediately(func, interval) {
+  func();
+  return setInterval(func, interval);
+}
+
+timerOn.startBtn.addEventListener('click', start);
+timerOn.stopBtn.addEventListener('click', stop);
+
